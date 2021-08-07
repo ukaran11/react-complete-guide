4e8e7fe4e083675a113568d6f300463de272e738
ukaran11 <ukaran11@gmail.com>
2021-08-08 02:27:22 +0530

    Utsav | Refactored components folder; converted normal functions to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
     const classes = 'card ' + props.className;
     // console.log(props)
     return <div className={classes}>{ props.children}</div>;
