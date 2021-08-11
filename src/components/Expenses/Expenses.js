4beaa0735c580d2b6a45fd0bc69709b8251ca987
ukaran11 <ukaran11@gmail.com>
2021-08-11 23:36:27 +0530

    Utsav | Rendered Dynamic lists instead of static ones

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,27 @@
+import { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
 import './Expenses.css';
 const Expenses = (props) => {
+  console.log(props);
+    const [filteredYear, setFilteredYear] = useState('2020'); 
     const filterChangeHandler = (selectedYear) => {
-      console.log(selectedYear);
+      setFilteredYear(selectedYear);
     };
-    
+
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter onChangeFilter = {filterChangeHandler}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   </div>
   );
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -10,11 +10,11 @@ const ExpensesFilter = (props) => {
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select onChange={ dropdownChangeHandler}>
+                <select value={props.selected} onChange={ dropdownChangeHandler}>
                     <option value='2022'>2022</option>
-                    <option value='2022'>2021</option>
-                    <option value='2022'>2020</option>
-                    <option value='2022'>2019</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
                 </select>
             </div>
         </div>
