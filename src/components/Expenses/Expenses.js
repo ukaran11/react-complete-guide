b78e1ebff2a107fa6e1fdda28d68067ee0b95fce
ukaran11 <ukaran11@gmail.com>
2021-08-12 23:44:37 +0530

    Utsav | Added key in dynamic list & Added conditon to filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,23 +5,34 @@ import ExpenseFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
 import './Expenses.css';
 const Expenses = (props) => {
-  console.log(props);
     const [filteredYear, setFilteredYear] = useState('2020'); 
+
     const filterChangeHandler = (selectedYear) => {
       setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    });
+    
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
-        {props.items.map((expense) => (
+        <ExpenseFilter 
+          selected={filteredYear} 
+          onChangeFilter = {filterChangeHandler}
+        />
+        {filteredExpenses.length === 0 ? (
+        <p>No expenses found.</p> 
+        ) : ( 
+        filteredExpenses.map((expense) => (
           <ExpenseItem
+            key ={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        )))}
     </Card>
   </div>
   );
